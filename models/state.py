1537924cfb82183b83cbbdfe8dfb1a35adc77626
ngari-kev <kevngariwangui@gmail.com>
2024-05-03 01:28:12 -0700

    Debugging

--- models/state.py
+++ models/state.py
@@ -11,8 +11,9 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    _cities = relationship("City", back_populates="state",
-                           cascade="all, delete-orphan")
+    cities = relationship("City", backref="state",
+                           cascade="delete")
+
 
     @property
     def cities(self):
