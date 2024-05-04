70939b36ea91b39b2dbe71353f554d6e2093cba6
ngari-kev <kevngariwangui@gmail.com>
2024-05-04 15:32:10 +0300

    amenity and place relationship

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    '''Represents the amenity class'''
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
+
+    place_amenities = Table('place_amenity', Base.metadata,
+                            Column('amenity_id', String(60),
+                                   ForeignKey('amenities.id'),
+                                   primary_key=True),
+                            Column('place_id', String(60),
+                                   ForeignKey('places.id'),
+                                   primary_key=True))
+
+    places = relationship("Place", secondary=place_amenities, viewonly=False)
