bce2b2bce1fa1aea4a7a1ff46a0bfd2313b017c9
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-05-16 07:36:23 +0300

    Debugging

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,11 +3,9 @@
 Fabric script to deploy and manage archives on web servers.
 """
 import os
-from fabric.api import env, local, run, lcd, cd
-
+from fabric.api import env, local, run, lcd, cd, sudo
 
 env.hosts = ['52.91.124.142', '52.91.118.158']
-
 
 def do_clean(number=0):
     """
@@ -19,27 +17,24 @@ def do_clean(number=0):
     Returns:
         True if clean up is successful, otherwise False.
     """
-    file_L = local('ls -t ~/AirBnB_Clone_V2/versions/').split()
-
-    path = "/data/web_static/releases"
-    with cd(path):
-        file_R = sudo("ls -t .").split()
-
     number = int(number)
     if number == 0:
-        num = 1
-    else:
-        num = number
+        number = 1
 
-    if len(file_R) > 0:
-        if len(file_L) == number or len(file_L) == 0:
-            pass
-        else:
-            count_local = file_L[num:]
-            for i in range(len(count_local)):
-                local("rm -f ~/AirBnB_Clone_V2/versions/{}".format(file_L[-1]))
-        count_remote = file_R[num:]
-        for j in range(len(count_remote)):
-            sudo("rm -rf {}/{}".format(path, count_remote[-1].strip(".tgz")))
-    else:
-        pass
+    # Clean local archives
+    local_archives = local('ls -t ~/AirBnB_clone_v2/versions', capture=True).split()
+    if len(local_archives) > number:
+        archives_to_delete = local_archives[number:]
+        with lcd('~/AirBnB_clone_v2/versions'):
+            for archive in archives_to_delete:
+                local("rm -f ./{}".format(archive))
+
+    # Clean remote archives
+    with cd('/data/web_static/releases'):
+        remote_archives = sudo('ls -t').split()
+        if len(remote_archives) > number:
+            archives_to_delete = remote_archives[number:]
+            for archive in archives_to_delete:
+                sudo('rm -rf ./{}'.format(archive))
+
+    return True
