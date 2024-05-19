38be0b772c6340439c3204bf2f652002dcf757a1
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-05-19 22:32:44 +0300

    Debugging

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -24,6 +24,6 @@ sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i "s/server_name _;/&\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t\tindex index.html;\n\t}\n/" /etc/nginx/sites-enabled/default
+sudo sed -i "s/server_name _;/&\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t\tindex index.html;\n\t}\n/" /etc/nginx/sites-available/default
 
 sudo service nginx restart
