429777fba0c21376cdb23fa559ca4ef2ca5b978b
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-06-05 14:05:29 +0300

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+# Declare a method to handle @app.teardown_appcontext
+@app.teardown_appcontext
+def teardown(exception):
+    """Closes the SQLAlchemy Session."""
+    storage.close()
+
+
+@app.route('/states_list')
+def HTMLPage():
+    """Displays a HTML page of list of states inside the tag body."""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+    #return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
