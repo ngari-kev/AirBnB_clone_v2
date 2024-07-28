62bb217e11bfb108ac1e3c4c91f9bfa310460586
ngari-kev <kevngariwangui@gmail.com>
2024-07-29 00:33:17 +0300

    with text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module starts a web application listening on 0.0.0.0, port 5000."""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Displays Hello HBNB."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_text(text):
+    """Displays C followed by text"""
+    text = text.replace('_', ' ')
+    return "C %s" % text
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
