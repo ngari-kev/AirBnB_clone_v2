a7f31710db8dc6cef5f69a49c4519d3465dd45dd
ngari-kev <kevngariwangui@gmail.com>
2024-07-28 23:57:07 +0300

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,5 +13,10 @@ def hello_world():
     return "Hello HBNB!"
 
 
+@app.route("/", strict_slashes=False)
+    """Displays HBNB"""
+    return "HBNB"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
