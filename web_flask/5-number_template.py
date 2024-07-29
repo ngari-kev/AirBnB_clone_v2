ee306efb7c7f068633655579826d5a578b7ff96b
ngari-kev <kevngariwangui@gmail.com>
2024-07-29 13:45:31 +0300

    import render_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module starts a web application listening on 0.0.0.0, port 5000."""
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
